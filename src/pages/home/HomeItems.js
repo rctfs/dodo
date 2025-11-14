dc9b1ab4af625c17ed38651514eb6739c8886e1d
rctfs <rui.torres.fernandes@gmail.com>
2025-11-14 01:23:00 +0000

    image optimization

--- src/pages/home/HomeItems.js
+++ src/pages/home/HomeItems.js
@@ -95,7 +95,7 @@ const items = [
 
     {
         id: "10",
-        img: "https://res.cloudinary.com/davcmtvbd/image/upload/v1763083214/atuacao2-min_vuobjk.jpg",
+        img: "https://res.cloudinary.com/davcmtvbd/image/upload/f_auto,q_auto,w_auto,dpr_auto/v1763083214/atuacao2-min_vuobjk.jpg",
         height: 550,
         position: "50% 40%",
         title: "Cronologia",
